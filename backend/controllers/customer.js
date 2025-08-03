a547bca1df6f1c2bbcf2ed7d856dfafc6a86d556
Aaruph <aaruph12345@gmail.com>
2025-08-03 16:46:40 +0545

    backendcompleteed

--- backend/controllers/customer.js
+++ backend/controllers/customer.js
@@ -11,6 +11,7 @@ const axios = require("axios");
 const jwt = require("jsonwebtoken");
 // 
 // 
+// 
 // Validation middleware for registration
 const validateRegister = [
   body("fname").trim().notEmpty().withMessage("First name is required"),
